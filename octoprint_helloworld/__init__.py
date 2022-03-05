c0353ca2ecd8cb52d7810a765acc7bacb6adfeee
tomekd85 <tomekdomachowski@wp.pl>
2022-03-05 23:35:05 +0100

    Initial commit of HelloWorldPlugin

---
+++ octoprint_helloworld/__init__.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+from __future__ import absolute_import, unicode_literals
+
+import octoprint.plugin
+
+
+class HelloWorldPlugin(octoprint.plugin.StartupPlugin):
+    def on_after_startup(self):
+        self._logger.info("Hello World!")
+
+
+__plugin_name__ = "Hello World"
+__plugin_pythoncompat__ = ">=2.7,<4"
+__plugin_implementation__ = HelloWorldPlugin()
