967175a72c4638fed76d3273736bac9afafb866d
tomekd85 <tomekdomachowski@wp.pl>
2022-03-29 23:00:44 +0200

    test commit for user

--- octoprint_helloworld/singalmessage/Sender.py
+++ octoprint_helloworld/singalmessage/Sender.py
@@ -3,7 +3,6 @@ from phoneData import phone
 # curl -X POST -H "Content-Type: application/json" 'http://localhost:8080/v2/send'
 # -d '{"message": "Test via Signal API!", "number": "+48504496169", "recipients": [ "+48504496169" ]}'
 
-
 if __name__ == '__main__':
     recipient = phone.get("recipient")
     sender = phone.get("sender")
