e2b367d635847752ebc90e2caf4eac2a901a7a04
tomekd85 <tomekdomachowski@wp.pl>
2022-04-03 01:28:46 +0200

    Remove comments

--- octoprint_helloworld/singalmessage/SignalMessageSender.py
+++ octoprint_helloworld/singalmessage/SignalMessageSender.py
@@ -1,7 +1,5 @@
 import requests
 from phoneData import phone
-# curl -X POST -H "Content-Type: application/json" 'http://localhost:8080/v2/send'
-# -d '{"message": "Test via Signal API!", "number": "+48504496169", "recipients": [ "+48504496169" ]}'
 
 
 class SignalMessageSender:
